455b17c0d1c5473db31a126b9694b6ad794ad190
Gucci Fan <gucc@umich.edu>
2023-04-12 21:36:58 -0400

    testing dummy routes to confirm database connection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create
+    end
+
+    def show
+        user = "hi" #User.find_by(id: session[:user_id])
+        render json: user
+    end
 end
