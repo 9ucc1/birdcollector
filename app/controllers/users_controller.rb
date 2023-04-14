07d2b1e531b50b4d344aaeb5f8fc908dfdf9148a
Gucci Fan <gucc@umich.edu>
2023-04-13 20:45:26 -0400

    create user route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
     def create
-        user = User.create
+        user = User.create(user_params)
+        render json: user, status: :created
     end
 
     def show
         user = "hi" #User.find_by(id: session[:user_id])
         render json: user
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    #The has_secure_password method also provides 
+    #two new instance methods on your User model: 
+    #password and password_confirmation. 
+    #These methods don't correspond to database columns! 
+    #Instead, to make these methods work, 
+    #your users table must have a password_digest column
 end
