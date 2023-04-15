3de74dd32925d24978bad5d1246fc730829702d8
Gucci Fan <gucc@umich.edu>
2023-04-14 21:37:17 -0400

    user show, sessions delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = "hi" #User.find_by(id: session[:user_id])
+        user = User.find_by(id: session[:user_id])
         render json: user
     end
 
