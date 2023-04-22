0cef73e5f20a479c75cc3371c5391f457142fa80
Gucci Fan <gucc@umich.edu>
2023-04-21 20:11:47 -0400

    birds render on client

--- client/src/Bird.js
+++ client/src/Bird.js
@@ -3,7 +3,11 @@ function Bird({birds}){
 
     const renderBird = birds.map(bird => (
         <div>
+            <img src={bird.image}/>
             {bird.com_name}
+            {bird.sci_name}
+            {bird.conservation_status}
+            {bird.description}
         </div>
     ))
 
